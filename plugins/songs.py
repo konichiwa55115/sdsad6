81e4e628c601338049b3a1cc391ad629e28e680e
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2022-12-29 22:24:18 +0200

    Update songs.py

--- plugins/songs.py
+++ plugins/songs.py
@@ -15,7 +15,7 @@ text = (
 descargar = Descargar('downloads/')
 
 @Medusa.on_message(
-    filters.command(['song'],prefixes=['/', '!'])
+    filters.command(['get'],prefixes=['/', '!'])
     & (filters.group | filters.private)
     & ~ filters.edited)
 async def song_dl(_, msg: Message):
